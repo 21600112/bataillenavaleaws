07529dcb4ce10021da42e1b2813194ca40dc37d0
billal zemmoura <billal.zemmoura@ens.uvsq.fr>
2020-03-10 16:05:38 +0100

    ajout du mécanisme de connexion

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+
+ 
+
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -27,7 +32,24 @@
         </nav>
 
     </div>
-
+<?php
+    $host = 'localhost';
+    $dbname = 'test';
+    $username = 'postgres';
+    $password = 'postgres';
+ 
+  $dsn = "pgsql:host=$host;port=5432;dbname=$dbname;user=$username;password=$password";
+   
+  try{
+     $conn = new PDO($dsn);
+     
+     if($conn){
+      echo "Connecté à $dbname avec succès!";
+     }
+  }catch (PDOException $e){
+     echo $e->getMessage();
+  }
+?>
     <h1> Bataille Navale </h1>
     <p>La bataille navale, appelée aussi touché-coulé, est un jeu de société dans lequel deux joueurs doivent placer des « navires » sur une grille tenue secrète et tenter de « toucher » les navires adverses. Le gagnant est celui qui parvient à couler tous les navires de l'adversaire avant que tous les siens ne le soient. On dit qu'un navire est coulé si chacune de ses cases a été touchées par un coup de l'adversaire. </p>
 
