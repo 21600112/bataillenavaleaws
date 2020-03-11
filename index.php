0039e77a6bc27222130b48aa024611d46f977090
billal zemmoura <billal.zemmoura@ens.uvsq.fr>
2020-03-11 10:02:37 +0100

    con

--- index.php
+++ index.php
@@ -32,13 +32,7 @@
         </nav>
 
     </div>
-<?php
-$dbconn = pg_connect("dbname=postgresql-cubic-62585");
-if ($dbconn)
-printf("établie );
-	
 
-?>
     <h1> Bataille Navale </h1>
     <p>La bataille navale, appelée aussi touché-coulé, est un jeu de société dans lequel deux joueurs doivent placer des « navires » sur une grille tenue secrète et tenter de « toucher » les navires adverses. Le gagnant est celui qui parvient à couler tous les navires de l'adversaire avant que tous les siens ne le soient. On dit qu'un navire est coulé si chacune de ses cases a été touchées par un coup de l'adversaire. </p>
 
